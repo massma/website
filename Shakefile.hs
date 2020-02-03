2c480927d1dee12f2599ae808a319ea4d2ee00c1
Adam Massmann <akm2203@columbia.edu>
2020-02-03 18:57:17 -0500

    index builds

--- Shakefile.hs
+++ Shakefile.hs
@@ -9,23 +9,18 @@ import           Development.Shake.Command
 import           Development.Shake.FilePath
 import           Development.Shake.Util
 
-css :: FilePath
-css = "pandoc.css"
+css :: String
+css = "http://www.columbia.edu/~akm2203/pandoc.css"
 
 main :: IO ()
 main = shakeArgs shakeOptions { shakeFiles = "_build" } $ do
   want (fmap ("public_html" </>) ["index.html"])
 
   "//*.html" %> \out -> do
-    let s = out -<.> "md"
-    need [s, "pandoc.css"]
-    cmd_ "pandoc"
-         ["-o", out, "-c", css, "--from", "markdown", "--to", "html5", s]
-
-  "//*.md" %> \out -> do
     let s = out -<.> "org"
     need [s]
-    cmd_ "pandoc" ["-o", out, "--from", "org", "--to", "markdown", s]
+    cmd_ "pandoc"
+         ["-s", "-o", out, "-c", css, "--from", "org", "--to", "html5", s]
 
   phony "clean" $ do
     liftIO $ putStrLn "Cleaning files in _build"

fb8d3a06a74e13eff1a63d0b284a5e7b173e58bf
Adam Massmann <akm2203@columbia.edu>
2020-02-04 19:34:45 -0500

    oops - forgot to add mnay files in last commit

--- Shakefile.hs
+++ Shakefile.hs
@@ -12,15 +12,39 @@ import           Development.Shake.Util
 css :: String
 css = "http://www.columbia.edu/~akm2203/pandoc.css"
 
+addHeader :: String -> String
+addHeader contents = unlines (header <> default' <> rest)
+ where
+  p xs = case xs of
+    (x1 : x2 : _xs) -> [x1, x2] == "#+"
+    _               -> False
+  (header, rest) = span p (lines contents)
+  default' =
+    [ "#+AUTHOR: Adam Massmann"
+    , "#+EMAIL: akm2203 \"at\" columbia \"dot\" edu"
+    , "#+LANGUAGE: en"
+    , "[[file:index.html][{Back to Home}]]"
+    ]
+
+htmlToSrc :: FilePath -> FilePath
+htmlToSrc h = "dat" </> takeFileName h -<.> "org"
+
 main :: IO ()
 main = shakeArgs shakeOptions { shakeFiles = "_build" } $ do
-  want (fmap ("public_html" </>) ["index.html"])
+  want
+    (fmap
+      ("public_html" </>)
+      ["index.html", "masters-research.html", "vpd-et.html", "writing.html"]
+    )
 
   "//*.html" %> \out -> do
-    let s = out -<.> "org"
+    let s      = htmlToSrc out
+    let parser = if takeFileName out == "index.html" then id else addHeader
     need [s]
-    cmd_ "pandoc"
-         ["-s", "-o", out, "-c", css, "--from", "org", "--to", "html5", s]
+    contents <- liftIO (parser <$> readFile s)
+    cmd_ (Stdin contents)
+         "pandoc"
+         ["-s", "-o", out, "-c", css, "--from", "org", "--to", "html5"]
 
   phony "clean" $ do
     liftIO $ putStrLn "Cleaning files in _build"
